e0e0fbc3e18a8d87226bffb1a55eeca696ce68bb
Orkhan Abbasaliev <orhan020700@mail.ru>
2022-11-22 16:15:54 +0300

    test
    test

---
+++ D6.6/NewsPaper/news/templatetags/custom_filters.py
@@ -0,0 +1,27 @@
+from django import template
+
+register = template.Library()
+
+@register.filter()
+def censor(value: str):
+
+    bad_words = {'Фигня': 'Ф****',
+                 'фигня': 'ф****'
+                 'Ерунда': 'Е*****',
+                 'ерунда': 'е*****',
+    }
+
+    words = value.split(' ')
+    new_text = ''
+
+    for i in range(len(words)):
+        if words[i] in bad_words:
+            words[i] = bad_words[words[i]]
+            new_text += words[i]
+            new_text += ' '
+        else:
+            new_text += words[i]
+            new_text += ' '
+
+
+    return new_text
